13bfe996aa4a75e8699a76e9bdc368b9a68d4f7e
AbidTom007 <atifanjum007@gmail.com>
2021-10-17 13:04:15 +0600

    ..

---
+++ MID_LAB_Exam set A/LOGIN.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login Page</title>
+</head>
+<body>
+<form method="post" action="../controller/logCheck.php">
+<fieldset>
+<legend>Login</legend>
+<table>
+<tr>
+<td>User Id</td>
+
+
+
+</tr>
+<tr>
+<td><input type="text" name="id" value=""> </td>
+</tr>
+<tr>
+<td>Password</td>
+
+
+
+</tr>
+<tr>
+<td><input type="password" name="password" value=""> </td>
+</tr>
+<tr>
+
+
+
+<td>
+<input type="submit" name="submit" value="Login">
+<a href="signup.html">Register</a>
+</td>
+</tr>
+</table>
+</fieldset>
+</form>
+</body>
+</html>
