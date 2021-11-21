94ebc0cfd4d09f37ac66cb5e0c331e643dab9690
AbidTom007 <atifanjum007@gmail.com>
2021-11-21 13:54:56 +0600

    Final lab-1

---
+++ FINAL_LAB_PERFORMANCE_1/Calculator.js
@@ -0,0 +1,30 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'CE':
+                display.innerText = '';
+                break;
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case 'DEL':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
